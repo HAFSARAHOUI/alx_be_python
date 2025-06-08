3916831080658b29e50e7bdaf454e0432c633210
Hafsa <hafsarahoui9@gmail.com>
2025-06-08 03:28:50 +0200

    fns4

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
-    elif operation == "subtract":
+    elif operation == "substract":
         return num1 - num2
     elif operation == "divide":
         if num2 == 0:
