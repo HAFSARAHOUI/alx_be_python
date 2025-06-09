141d75bfe6a42c7bee3324a00f87c81e78bf2fca
Hafsa <hafsarahoui9@gmail.com>
2025-06-09 18:35:48 +0200

    alx

--- fns_and_dsa/0x00-python_variable_annotations/2-floor.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Returns the floor of a float number."""
-
-
-def floor(n: float) -> int:
-    """Returns the floor of a float number."""
-    return int(n)
--- fns_and_dsa/0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Returns the string representation of a float."""
-
-
-def to_str(n: float) -> str:
-    """Returns the string representation of a float."""
-    return str(n)
